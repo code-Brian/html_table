1a0293e5faa07721e7475b9d65bbe286cb902e0e
Brian Denmark <testingarea479@gmail.com>
2022-08-21 22:33:11 -0700

    added basic bootstrap to style table

--- server.py
+++ server.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route('/tabletime')
 def make_table():
     users = [
-        {'first_name' : 'Michael', 'last_name' : 'Choi'},
-        {'first_name' : 'John', 'last_name' : 'Supsupin'},
-        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+        {'user_id': 1,'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'user_id': 2,'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'user_id': 3,'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'user_id': 4,'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
     return render_template('tabletime.html', users=users)
 
